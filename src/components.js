10922e80e63b97f3e4ba19dabe14733c2964e27a
molgin <mollygingras@gmail.com>
2016-09-15 19:17:41 -0400

    CrosswordSquares get their props from the store and show clue numbers

--- src/components.js
+++ src/components.js
@@ -5,20 +5,13 @@ import React from 'react';
 export default class CrosswordGrid extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
-    // just making a multidimensional array
-    this.state.squares = Array.apply(null, { length: this.props.width })
-      .map( (e, row) => Array.apply(null, { length: this.props.height })
-        .map((square, col) => ({row, col, id: row * this.props.width + col, solution: null}))
-      );
-    console.log(this.state.squares.reduce((a,b) => a.concat(b)));
   }
 
   render() {
     return (
-      <table id="crosswordGrid">
+      <table id="crossword-grid">
         <tbody>
-          {this.state.squares.map((row, i) => <tr key={i}>{row.map((square,j)=><td className="crosswordSquare" key={j}><CrosswordSquare row={square.row} col={square.col} /></td>)}</tr>)}
+          {this.props.squares.map((row, i) => <tr key={i}>{row.map((square,j)=><td className="crossword-square" key={j}><CrosswordSquare row={square.row} col={square.col} clueNumber={square.clueNumber} solution={square.solution} /></td>)}</tr>)}
         </tbody>
       </table>
     );
@@ -30,9 +23,15 @@ export class CrosswordSquare extends React.Component {
   constructor(props) {
     super(props);
   }
+  
   render() {
     return (
-      <input type="text" value={this.props.col + ", " + this.props.row} />
+      <div>
+        <div className="clue-number-wrapper">
+          <div className="clue-number">{this.props.clueNumber}</div>
+        </div>
+        <input type="text" value={this.props.solution} />
+      </div>
     );
   }
 }
--- src/reducers.js
+++ src/reducers.js
@@ -1,6 +1,28 @@
+const width = 10
+const height = width
+
+let squares = Array.apply(null, { length: width })
+  .map((e, row) => Array.apply(null, { length: height })
+    .map((square, col) => {
+      let clueNumber
+      if (row === 0)
+        clueNumber = col + 1
+      else if (col === 0)
+        clueNumber = row + width
+
+      return {
+        row,
+        col,
+        clueNumber,
+        solution: undefined,
+        enabled: true,
+      }
+    })
+)
+
 const initialState = {
   clues: [],
-  squares: [],
+  squares,
   settings: {}
 }
 
